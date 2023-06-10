60ce14abffa1a23dc230431a483114bef5dbd453
knirantar <knirantar@gmail.com>
2023-06-10 14:56:55 +0530

    Added the template directory and URL for views

--- sportsBlog/sports/views.py
+++ sportsBlog/sports/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+# Create your views here
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'sports/post/list.html', {'posts': posts})
+
+
+def post_details(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'sports/post/detail.html', {'post': post})
